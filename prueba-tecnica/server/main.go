2a43774d8307623f02b9f1da5368ac55f10acae3
Adolfo Chafardett <adolfo1997a@gmail.com>
2024-03-16 08:50:59 -0400

    feat: updated main.go file

--- prueba-tecnica/server/main.go
+++ prueba-tecnica/server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	CSV "api/packages/csv"
 	Response "api/packages/response"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -24,6 +25,27 @@ func main() {
 	})
 
 	router.GET("/csv", func(c *gin.Context) {
+
+		var queryParams struct {
+			FirstName        string
+			LastName         string
+			City             string
+			Email            string
+			SubscriptionDate string
+		}
+
+		if err := c.ShouldBindQuery(&queryParams); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": "error",
+				"error":   err.Error(),
+				"code":    1,
+			})
+			return
+		}
+
+		queryParamsData := c.Request.URL.Query()
+		fmt.Println(queryParamsData)
+
 		csv := CSV.ReadCsvFile("../data/customers-100000.csv")
 		records := Response.Paginate(csv.Records, 1, 10, len(csv.Records))
 		headers := csv.Headers
